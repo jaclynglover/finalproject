028957af2913113a6aef51c5bcbe1cee137e3320
jg1565 <jg1565@georgetown.edu>
2017-07-23 17:48:36 -0400

    Added pre_get_posts hook to functions.php file

--- wp-content/themes/retina-child/functions.php
+++ wp-content/themes/retina-child/functions.php
@@ -10,5 +10,15 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
 }
+
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
+
+function add_my_post_types_to_query( $query ) {
+  if ( is_home() && $query->is_main_query() )
+    $query->set( 'post_type', array( 'post', 'glover_runs' ) );
+  return $query;
+}
+
 ?>
