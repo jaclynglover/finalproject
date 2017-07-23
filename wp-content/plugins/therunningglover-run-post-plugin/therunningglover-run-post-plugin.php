524ddd621bb8ef2be415735c8a675e763312c918
jg1565 <jg1565@georgetown.edu>
2017-07-23 15:11:42 -0400

    Fixed code in custom post type PHP file

--- wp-content/plugins/therunningglover-run-post-plugin/therunningglover-run-post-plugin.php
+++ wp-content/plugins/therunningglover-run-post-plugin/therunningglover-run-post-plugin.php
@@ -6,35 +6,20 @@ Author: Jaclyn Glover
 Author URI: www.therunningglover.com
 */
 
-defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
-
-// custom post type function
-
-function create_posttype() {
-
-	register_post_type( 'runs',
-		array(
-			'labels' => array(
-				'name' => ( 'Runs' ),
-				'singular_name' => ( 'Run' )
-			),
-			'public' => true,
-			'has_archive' => true,
-			'rewrite' => array('slug' => 'runs'),
-		)
-	);
-
-add_action( 'init', 'create_posttype' );
-
-add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
-
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+  register_post_type( 'glover_runs',
+    array(
+      'labels' => array(
+        'name' => __( 'Runs' ),
+        'singular_name' => __( 'Run' )
+      ),
+      'public' => true,
+      'show_in_menu' => true,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'runs'),
+    )
+  );
 }
-
-function add_my_post_types_to_query( $query ) {
-	if ( is_home() && $query->is_main_query() )
-		$query->set( 'post_type', array( 'post', 'runs' ) 
-);
-	return $query;
-} 
 
 ?>
