b1104e68d97160230ac4e5964ab64179c49fc9b2
jg1565 <jg1565@georgetown.edu>
2017-07-23 23:39:42 -0400

    Created custom Run Details metabox and altered child theme style.css

--- wp-content/plugins/therunningglover-run-post-plugin/therunningglover-run-post-plugin.php
+++ wp-content/plugins/therunningglover-run-post-plugin/therunningglover-run-post-plugin.php
@@ -6,20 +6,38 @@ Author: Jaclyn Glover
 Author URI: www.therunningglover.com
 */
 
+// below portion of code based on WordPress codex at https://codex.wordpress.org/Post_Types
 add_action( 'init', 'create_post_type' );
 function create_post_type() {
-  register_post_type( 'glover_runs',
+  register_post_type( 'glover_run',
     array(
       'labels' => array(
-        'name' => __( 'Runs' ),
-        'singular_name' => __( 'Run' )
+        'name' => __( 'Run' ),
+        'singular_name' => __( 'Run' ),
       ),
       'public' => true,
       'show_in_menu' => true,
       'has_archive' => true,
-      'rewrite' => array('slug' => 'runs'),
+      'taxonomies' => array('category', 'post_tag'),
+      'rewrite' => array('slug' => 'run'),
     )
   );
 }
 
+// below portion of code based on example from https://www.sitepoint.com/adding-custom-meta-boxes-to-wordpress/
+
+function custom_meta_box_markup()
+{
+    
+}
+
+function add_custom_meta_box()
+{
+    add_meta_box("run-meta-box", "Run Details", "custom_meta_box_markup", "glover_run", "side", "high", null);
+}
+
+add_action("add_meta_boxes", "add_custom_meta_box");
+
 ?>
+
+
