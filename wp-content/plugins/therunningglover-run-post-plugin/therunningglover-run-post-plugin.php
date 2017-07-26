ce95dd5b831218c7b95c2beb6f651044ab0cb3b9
jg1565 <jg1565@georgetown.edu>
2017-07-25 21:27:38 -0400

    Added custom fields to custom post type plugin

--- wp-content/plugins/therunningglover-run-post-plugin/therunningglover-run-post-plugin.php
+++ wp-content/plugins/therunningglover-run-post-plugin/therunningglover-run-post-plugin.php
@@ -19,6 +19,7 @@ function create_post_type() {
       'show_in_menu' => true,
       'has_archive' => true,
       'taxonomies' => array('category', 'post_tag'),
+      'supports' => array('title','editor','author','excerpt','comments','revisions'),
       'rewrite' => array('slug' => 'run'),
     )
   );
@@ -26,18 +27,32 @@ function create_post_type() {
 
 // below portion of code based on example from https://www.sitepoint.com/adding-custom-meta-boxes-to-wordpress/
 
-function custom_meta_box_markup()
-{
-    
-}
-
 function add_custom_meta_box()
 {
-    add_meta_box("run-meta-box", "Run Details", "custom_meta_box_markup", "glover_run", "side", "high", null);
+  add_meta_box("run-meta-box", "Run Details", "custom_meta_box_markup", "glover_run", "side", "high", null);
 }
 
 add_action("add_meta_boxes", "add_custom_meta_box");
 
+function custom_meta_box_markup($object)
+{
+  wp_nonce_field(basename(__FILE__), "meta-box-nonce");
+  
+  ?>
+    <div>
+      <label for="neighborhood_meta_box_text">Neighborhood</label>
+      <input name="neighborhood_meta_box_text" type="text" value="<?php echo get_post_meta($object->ID, "neighborhood-box-text", true); ?>">
+
+      <br>
+
+      <label for="date_meta_box_text">Date</label>
+      <input name="date_meta_box_text" type="text" value="<?php echo get_post_meta($object->ID, "date-box-text", true); ?>">
+
+      <br>
+
+      <label for="time_meta_box_text">Time</label>
+      <input name="time_meta_box_text" type="text" value="<?php echo get_post_meta($object->ID, "time-box-text", true); ?>">
+    </div>
+  <?php    
+}
 ?>
-
-
