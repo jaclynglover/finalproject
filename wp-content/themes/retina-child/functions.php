2bcea2e20bbb628fbfd063a839826053e61fd7b0
jg1565 <jg1565@georgetown.edu>
2017-07-28 23:25:23 -0400

    Recreated child theme folder and files due to FTP issue

--- wp-content/themes/retina-child/functions.php
+++ wp-content/themes/retina-child/functions.php
@@ -1,7 +1,7 @@
 <?php
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'child-style'; // This is 'retina-style' for the Retina theme.
+    $parent_style = 'child-style'; // This is 'child-style' for the Retina theme.
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
@@ -12,6 +12,8 @@ function my_theme_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+// following block of code modeled after https://codex.wordpress.org/Plugin_API/Action_Reference/pre_get_posts
 
 add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
 
