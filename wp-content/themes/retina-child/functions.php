2f2c3c05094454daf0eac5d0a3bd1aa63bafe53e
jg1565 <jg1565@georgetown.edu>
2017-07-29 15:41:15 -0400

    Edited child theme styles - fonts, colors, buttons

--- wp-content/themes/retina-child/functions.php
+++ wp-content/themes/retina-child/functions.php
@@ -1,8 +1,9 @@
 <?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-
-    $parent_style = 'child-style'; // This is 'child-style' for the Retina theme.
-
+ 
+    $parent_style = 'parent-style'; // This is 'child-style' for the Retina theme.
+ 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
@@ -10,8 +11,6 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
 }
-
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 // following block of code modeled after https://codex.wordpress.org/Plugin_API/Action_Reference/pre_get_posts
 
@@ -26,7 +25,7 @@ function add_my_post_types_to_query( $query ) {
 // Google Fonts enqueue code below is from https://www.tipsandtricks-hq.com/how-to-easily-add-google-web-fonts-to-your-wordpress-theme-4915
 
 function load_google_fonts() {
-wp_register_style('googleWebFonts', 'http://fonts.googleapis.com/css?family=Heebo|Cardo|BenchNine');
+wp_register_style('googleWebFonts', 'http://fonts.googleapis.com/css?family=Bentham|Lato|Cardo');
 wp_enqueue_style('googleWebFonts');
 }
 
